a26f3c663325cc9c246a4f8126e2eec35038de71
krish-98 <muralikrishnanm98@gmail.com>
2021-10-05 16:16:09 +0530

    created 1 app out of 4

---
+++ app.js
@@ -0,0 +1,16 @@
+const inputOne = document.querySelector("#input-1")
+const inputTwo = document.querySelector("#input-2")
+const inputThree = document.querySelector("#input-3")
+const checkTriangle = document.querySelector("#check-triangle") 
+const showMessage = document.querySelector("#show-message")
+
+checkTriangle.addEventListener("click", () => {
+  
+  const userTriangle = Number(inputOne.value) + Number(inputTwo.value) + Number(inputThree.value)
+
+  if (userTriangle === 180) { 
+    showMessage.innerText = "Yay!, that's make a triangle"
+  } else {
+    showMessage.innerText = "Oops!, that doesn't make a proper triangle"
+  }
+})
