e2fe07ce51205bb0bef361f5a1502eb4cddfa995
krish-98 <muralikrishnanm98@gmail.com>
2021-10-07 12:48:52 +0530

    folder structuring

---
+++ scripts/hypotenuse.js
@@ -0,0 +1,10 @@
+const baseValue = document.querySelector("#base-value")
+const heightValue = document.querySelector("#height-value")
+const calculateHypo = document.querySelector("#calculate-hypo")
+const showMessage = document.querySelector("#show-message")
+
+
+calculateHypo.addEventListener("click", () => {
+  console.log("button clicked")
+})
+
